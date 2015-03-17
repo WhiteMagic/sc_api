0b8440e43e11cdc02b3e59eb663e3433c81b4c28
Lionel Ott <white.magic@gmx.de>
2015-03-18 10:58:28 +1100

    Update with daily delta statistics

--- sc_scraper.py
+++ sc_scraper.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 import argparse
+import datetime
 import multiprocessing
 import os
 import pickle
 import requests
 import sys
+import time
 
 
 
@@ -48,7 +50,7 @@ class Task(object):
             params=payload,
             proxies=self.proxies
         )
-        print("Processing {}".format(self.handle))
+        #print("Processing {}".format(self.handle))
         return (self.handle, r.json()["data"]["resultset"][0])
 
 
@@ -77,6 +79,14 @@ class Pilot(object):
     @property
     def score(self):
         return int(self._data["score"])
+
+    @property
+    def deaths(self):
+        return int(self._data["deaths"])
+
+    @property
+    def kills(self):
+        return int(self._data["kills"])
 
     @property
     def rank(self):
@@ -117,6 +127,52 @@ class Pilot(object):
         return (data["name"], float(data["ratio"]))
 
 
+class DeltaEntry(object):
+
+    def __init__(self, pilot, data):
+        self.date = datetime.datetime.utcnow().date()
+        tmp = Pilot(data)
+        self.deaths = tmp.deaths - pilot.deaths
+        self.flight_time = tmp.flight_time - pilot.flight_time
+        self.kills = tmp.kills - pilot.kills
+        self.matches = tmp.matches - pilot.matches
+        self.score = tmp.score - pilot.score
+
+
+class Storage(object):
+
+    def __init__(self):
+        self._storage = {0: {}}
+
+    def date_keys(self):
+        keys = list(self._storage.keys())
+        del keys[0]
+        return keys
+
+    def pilot_exists(self, handle):
+        return handle in self._storage[0]
+
+    def get_data(self, date=0):
+        return self._storage[date]
+
+    def get_pilot(self, handle, date=0):
+        return self._storage[date][handle]
+
+    def update_pilot(self, handle, data):
+        if handle not in self._storage[0]:
+            self._storage[0][handle] = Pilot(data)
+        else:
+            self._create_delta_entry(handle, data)
+            self._storage[0][handle].update(data)
+
+    def _create_delta_entry(self, handle, data):
+        delta = DeltaEntry(self._storage[0][handle], data)
+        if delta.date not in self._storage:
+            self._storage[delta.date] = {}
+        self._storage[delta.date][handle] = delta
+        print(len(self._storage[delta.date]))
+
+
 def query_boundaries(url, query, proxies={}):
     r = requests.post(url, proxies=proxies, params=query)
     data = r.json()["data"]
@@ -127,7 +183,7 @@ def query_boundaries(url, query, proxies={}):
     }
 
 
-def scrape_leaderboard(pilots, mode, season=5, proxies={}):
+def scrape_leaderboard(storage, mode, season=5, proxies={}):
     leaderboard_url = "https://robertsspaceindustries.com/api/arena-commander/getLeaderboard"
     query = {
         "map": "MAP-ANY",
@@ -152,28 +208,31 @@ def scrape_leaderboard(pilots, mode, season=5, proxies={}):
 
         for i, entry in enumerate(req.json()["data"]["resultset"]):
             handle = entry["nickname"]
-            if handle not in pilots:
-                print("New pilot     : {}".format(handle))
-                pilots[handle] = Pilot(entry)
+            if not storage.pilot_exists(handle):
+                #print("New pilot     : {}".format(handle))
+                storage.update_pilot(handle, entry)
                 tasks.put(Task(handle, mode, proxies))
             else:
                 tmp = Pilot(entry)
-                if tmp.flight_time > pilots[handle].flight_time:
-                    print("Updating pilot: {}".format(handle))
+                if tmp.flight_time > storage.get_pilot(tmp.handle).flight_time:
+                    #print("Updating pilot: {}".format(handle))
                     tasks.put(Task(handle, mode, proxies))
-                pilots[handle].update(entry)
+                    storage.update_pilot(tmp.handle, entry)
 
+    time.sleep(10)
+    print("Inserting poison pill")
     for entry in data_grabbers:
         tasks.put(None)
 
     print("Waiting for detailed data to be grabbed")
     tasks.join()
+    print("Inserting data")
     while not results.empty():
         entry = results.get()
-        pilots[entry[0]].update(entry[1])
+        storage.get_pilot(entry[0]).update(entry[1])
 
 
-def scrape_data(pilot_data, proxies):
+def scrape_data(data, proxies):
     mode_list = {
         "Battle Royale": "BR",
         "Squadron Battle": "SB",
@@ -183,9 +242,9 @@ def scrape_data(pilot_data, proxies):
 
     for mode in mode_list.values():
         print(">>> Processing mode: {}".format(mode))
-        if mode not in pilot_data:
-            pilot_data[mode] = {}
-        scrape_leaderboard(pilot_data[mode], mode, proxies)
+        if mode not in data:
+            data[mode] = Storage()
+        scrape_leaderboard(data[mode], mode, proxies)
 
 
 def main():
@@ -201,11 +260,11 @@ def main():
     }
     
     # Load data and scrape data
-    pilot_data = {}
+    data = {}
     if os.path.exists(args.data_file):
-        pilot_data = pickle.load(open(args.data_file, "rb"))
-    scrape_data(pilot_data, proxies=proxies)
-    pickle.dump(pilot_data, open(args.data_file, "wb"))
+        data = pickle.load(open(args.data_file, "rb"))
+    scrape_data(data, proxies=proxies)
+    pickle.dump(data, open(args.data_file, "wb"))
 
 
     return 0
